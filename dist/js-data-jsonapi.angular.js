93d1d9d4527147e22bcc3964a4ddf70741c56dd3
Mark Lawlor <mark.lawlor@bullioncapital.com>
2016-02-05 15:38:54 +1000

    Add serialize and change DSUpdate to use PATCH

--- dist/js-data-jsonapi.angular.js
+++ dist/js-data-jsonapi.angular.js
@@ -1,148 +1,145 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var DSHttpAdapter = require('js-data-http');
+var DSHttpAdapter = require('js-data-http')
 
-function DSJsonApiAdapter(options) {
-	var defaultOptions = {
-		serialize: DSJsonApiAdapter.serialize,
-		deserialize: DSJsonApiAdapter.deserialize,
-		queryTransform: DSJsonApiAdapter.queryTransform
-	};
+function DSJsonApiAdapter (options) {
+  var defaultOptions = {
+    serialize: DSJsonApiAdapter.serialize,
+    deserialize: DSJsonApiAdapter.deserialize,
+    queryTransform: DSJsonApiAdapter.queryTransform
+  }
 
-	options = _.extend({}, defaultOptions, options);
+  options = _.extend({}, defaultOptions, options)
 
-	this.defaults = options;
-    this.http = new DSHttpAdapter(options);
+  this.defaults = options
+  this.http = new DSHttpAdapter(options)
 }
 
 /*	==========================================================================
 	 Prototype Methods
 	========================================================================== */
 DSJsonApiAdapter.prototype.create = function (definition, attrs, options) {
-    return this.http.create(definition, attrs, options);
-};
+  return this.http.create(definition, attrs, options)
+}
 
 DSJsonApiAdapter.prototype.find = function (definition, id, options) {
-    return this.http.find(definition, id, options);
-  // Must resolve the promise with the found item
-};
+  return this.http.find(definition, id, options)
+}
 
 DSJsonApiAdapter.prototype.findAll = function (definition, params, options) {
-    return this.http.findAll(definition, params, options);
-  // Must resolve the promise with the found items
-};
+  return this.http.findAll(definition, params, options)
+}
 
 DSJsonApiAdapter.prototype.update = function (definition, id, attrs, options) {
-    return this.http.update(definition, id, attrs, options);
-  // Must resolve the promise with the updated item
-};
+  options.method = 'PATCH'
+  return this.http.update(definition, id, attrs, options)
+}
 
 DSJsonApiAdapter.prototype.updateAll = function (definition, attrs, params, options) {
-    return this.http.updateAll(definition, attrs, params, options);
-  // Must resolve the promise with the updated items
-};
+  return this.http.updateAll(definition, attrs, params, options)
+}
 
 DSJsonApiAdapter.prototype.destroy = function (definition, id, options) {
-    return this.http.destroy(definition, id, options);
-  // Must return a promise
-};
+  return this.http.destroy(definition, id, options)
+}
 
 DSJsonApiAdapter.prototype.destroyAll = function (definition, params, options) {
-    return this.http.destroyAll(definition, params, options);
-  // Must return a promise
-};
+  return this.http.destroyAll(definition, params, options)
+}
 
 /*	==========================================================================
 	 Static Methods
 	========================================================================== */
-DSJsonApiAdapter.serialize = function serialize(resourceConfig, data){
-    console.log('serialize', data);
-    return data;
-};
+DSJsonApiAdapter.serialize = function serialize (resourceConfig, data) {
+  return {
+    data: {
+      id: data.id,
+      type: resourceConfig.name,
+      attributes: _.omit(data, ['id'])
+    }
+  }
+}
 
-DSJsonApiAdapter.deserialize = function(resourceConfig, response){
-    // Add the include data first
-    if(_.has(response, 'data.include')){
-    	if( !Array.isArray( response.data.include ) ) {
-    		response.data.include = [response.data.include];
-    	}
-
-        response.data.include.map( injectInclude.bind(this, resourceConfig) );
+DSJsonApiAdapter.deserialize = function (resourceConfig, response) {
+  // Add the include data first
+  if (_.has(response, 'data.include')) {
+    if (!Array.isArray(response.data.include)) {
+      response.data.include = [response.data.include]
     }
 
-    var deserializedData = null;
+    response.data.include.map(injectInclude.bind(this, resourceConfig))
+  }
 
-    if(_.has(response, 'data.data')){
-    	if( !Array.isArray( response.data.data ) ) {
-    		response.data.data = [response.data.data];
-    	}
+  var deserializedData = null
 
-        deserializedData = response.data.data.map( deserializeData.bind(this, resourceConfig) );
+  if (_.has(response, 'data.data')) {
+    if (!Array.isArray(response.data.data)) {
+      response.data.data = [response.data.data]
     }
 
-    return deserializedData;
-};
+    deserializedData = response.data.data.map(deserializeData.bind(this, resourceConfig))
+  }
 
-DSJsonApiAdapter.queryTransform = function(resourceConfig, params) {
-    var returnParams = {};
+  return deserializedData
+}
 
-    if( params.offset ) _.set(returnParams, 'offset', params.offset);
-    if( params.limit ) _.set(returnParams, 'limit', params.limit);
-    if( params.where ) {
-        _.forEach( params.where, function( value, key ){
-            _.set(returnParams, key, value);
-        });
-    }
+DSJsonApiAdapter.queryTransform = function (resourceConfig, params) {
+  var returnParams = {}
 
-    return returnParams;
-};
+  if (params.offset) _.set(returnParams, 'offset', params.offset)
+  if (params.limit) _.set(returnParams, 'limit', params.limit)
+  if (params.where) {
+    _.forEach(params.where, function (value, key) {
+      _.set(returnParams, key, value)
+    })
+  }
+
+  return returnParams
+}
 
 /*	==========================================================================
 	 Helper Functions
 	========================================================================== */
-function deserializeData(resourceConfig, data){
-    if(!data){
-        return;
-    }
+function deserializeData (resourceConfig, data) {
+  if (!data) {
+    return
+  }
 
-    if(data.relationships){
-        _.forEach(data.relationships, function(value){
-            var relations = [].concat(value.data);
+  if (data.relationships) {
+    _.forEach(data.relationships, function (value) {
+      var relations = [].concat(value.data)
 
-            _.forEach(relations, function(relation){
-                if( !data[ relation.type + 'Ids' ] ){
-                    data[ relation.type + 'Ids' ] = [];
-                }
+      _.forEach(relations, function (relation) {
+        if (!data[ relation.type + 'Ids' ]) {
+          data[ relation.type + 'Ids' ] = []
+        }
 
-                data[ relation.type + 'Ids' ].push( relation.id );
-            });
-        });
+        data[ relation.type + 'Ids' ].push(relation.id)
+      })
+    })
 
-        delete data.relationships;
-    }
+    delete data.relationships
+  }
 
-    // Move the attributes to the top level
-    _.extend(data, data.attributes);
+  // Move the attributes to the top level
+  _.extend(data, data.attributes)
+  delete data.attributes
 
-    // Remove the type
-    if (!data.attributes.type) {
-    delete data.type;
-    }
+  // Remove the type
+  delete data.type
 
-    delete data.attributes;
-
-    return data;
+  return data
 }
 
-function injectInclude(resourceConfig, data){
-    data = deserializeData(data);
+function injectInclude (resourceConfig, data) {
+  data = deserializeData(data)
 
-    var type = data.type;
-    delete data.type;
+  var type = data.type
+  delete data.type
 
-    resourceConfig.getResource(type).inject(data);
+  resourceConfig.getResource(type).inject(data)
 }
 
-module.exports = DSJsonApiAdapter;
+module.exports = DSJsonApiAdapter
 
 },{"js-data-http":2}],2:[function(require,module,exports){
 /*!
